fc014fd42c0206861c3d07483ae7e19033e93d39
Oracle Global Leaders Program <reiner.zimmermann@oracle.com>
2023-05-07 16:23:46 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -47,13 +47,18 @@ streamlit.write('The user entered ', fruit_choice)
 
 #don´t run anything past here while we troubleshoot
 streamlit.stop()
-
-my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
-my_cur = my_cnx.cursor()
-my_cur.execute("select * from fruit_load_list")
-my_data_rows = my_cur.fetchall()
 streamlit.header("The fruit load list contains:")
-streamlit.dataframe(my_data_rows)
+#Snowflake-related functions
+def get_fruit_load_list():
+    with my_cnx.cursor() as my_cur: 
+         my_cur.execute("select * from fruit_load_list")      
+         return my_cur.fetchall()
+
+#add a button to load the fruits
+if streamlit.button('Get Fruit Load List'):
+    my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
+    my_data_rows = get_fruit_load_list()
+    streamlit.dataframe(my_data_rows)
 
 # Let's put a pick list here so they can pick the fruit that should be added to the list
 add_my_fruit = streamlit.multiselect("Which fruit would you like to add:", list(my_fruit_list.index),['Avocado','Strawberries'])
