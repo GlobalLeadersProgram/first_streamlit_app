f3231af2774940e258e4259b9af8de4309a7da0f
Oracle Global Leaders Program <reiner.zimmermann@oracle.com>
2023-05-07 11:27:04 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -26,8 +26,8 @@ streamlit.dataframe(fruits_to_show)
 streamlit.header("Fruityvice Fruit Advice!")
 
 import requests
-fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
-streamlit.text(fruityvice_response.json())
+fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + "kiwi")
+# take the json version of the response and normalize it
 
 # normalize fruityvice output into a table
 fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
