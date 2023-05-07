23d083373ca4bd35d4cecec7a06dc13f176c12ba
Oracle Global Leaders Program <reiner.zimmermann@oracle.com>
2023-05-07 16:15:11 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -26,16 +26,19 @@ streamlit.dataframe(fruits_to_show)
 
 # New Section to display fruityvice api response
 streamlit.header("Fruityvice Fruit Advice!")
-fruit_choice = streamlit.text_input('What fruit would you like information about?','Kiwi')
+try:
+   fruit_choice = streamlit.text_input('What fruit would you like information about?','Kiwi')
+   if not fruit_choice:
+      streamlit.error("Please select a fruit to get information.")
+   else:
+      fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_choice)
+      fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())    
+      streamlit.dataframe(fruityvice_normalized)
+      
+except URLError as e:
+   streamlit.error()
+      
 streamlit.write('The user entered ', fruit_choice)
-
-fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_choice)
-# take the json version of the response and normalize it
-
-# normalize fruityvice output into a table
-fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
-# show output into a data frame 
-streamlit.dataframe(fruityvice_normalized)
 
 #don´t run anything past here while we troubleshoot
 streamlit.stop()
